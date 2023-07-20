53e6d471268806e3b02ac09001d4c27292f0d5df
David Suarez <davidsuarezlosada@gmail.com>
2023-07-20 12:53:03 -0500

    feat: componente para adivinanza
    Se crea componente de adivinanzas, retos de suiza y paris

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,41 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent {
   
+  isCorrectAnswer: boolean = false;
   incorrectAnswer: boolean = false;
   days: number = 0;
-  message:string = `En tierras lejanas, sueños dorados,<br>
-  Una dama sublime, París en sus manos.<br>
-  Maestra de inglés, su esencia divina,<br>
-  Conquistó mi ser, mi musa parisina.<br>
-  <br>
-  En la clase de idiomas, un reto dejó,<br>
-  Aprender una canción que al corazón llegó.<br>
-  Melodía envolvente, un himno al compás,<br>
-  Despertando anhelos en cada compás.<br>
-  <br>
-  Sus ojos reflejaban luces en el Sena,<br>
-  Susurros de enseñanza, magia plena.<br>
-  París, su pasión, su inspiración sin par,<br>
-  En cada rincón, su esencia a brillar.<br>
-  <br>
-  Enigmas que guarda, el firmamento estelar,<br>
-  Misterios del cosmos, esperan desvelar.<br>
-  Dime, dama espléndida, sin igual,<br>
-  ¿Qué secreto ocultan las estrellas en su palmar?`;
+  message:string = `En los senderos de la vida he andado, <br>
+  cosechando momentos, experiencias de lado a lado.<br>
+  Personas amadas, almas que me han acompañado,<br>
+  en mi camino, siempre, tu amor ha brillado.<br><br>
+  
+  Un secreto guardo en mis labios sellado,<br>
+  una expresión de gratitud, un tesoro ocultado.<br>
+  En momentos de gozo y en días nublados,<br>
+  esta frase se alza como un canto entonado.<br><br>
+  
+  Es un tributo sincero, un agradecimiento profundo,<br>
+  un reconocimiento que en palabras se difunde.<br>
+  Cada instante compartido, cada momento vivido,<br>
+  gracias por tanto, por las heridas que has removido.<br><br>
+  
+  Enigmas te invito a resolver, con cariño te reto,<br>
+  ¿Cuál es la frase que, con amor, agradezco en secreto?`;
 
   isDiscover: boolean = false;
   targetDate: Date = new Date('2023-07-15 17:00:00');
   inputText: string = '';
   
-
-
-
   validateText() {
     const lowercaseInput = this.inputText.toLowerCase().trim();
-    const validText = 'counting stars';
-
+    const validText = 'gracias por todo';
+console.log(123)
     if (lowercaseInput === validText) {
-      this.message = "Felicidades amore mío!!!, esta es tu siguiente pista: Atelier" ;
+      this.message = "mercredi 26 juil." ;
+      this.isCorrectAnswer= true;
     } else {
       this.incorrectAnswer = true;
     }
